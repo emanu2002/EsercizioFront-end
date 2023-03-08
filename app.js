4343a29808cf6dd34d002bc9f23158aa737baa48
Emanuele Carlucci <emanuele.carlucci100@edu.unito.it>
2023-03-08 18:38:10 +0100

    finto login e colori aside

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ var app = new Vue({
     el: '#spa',
     data: {
         paginaC:"Home",
+        login: false,
     },
     methods: {
         paginaCorrente: function(paginaNuova){
             var self=this;
-            console.log("vue #"+ self.paginaC);
-            console.log("vecc #"+ paginaNuova);
             document.getElementById(paginaNuova).style.backgroundColor = "grey";
             document.getElementById(self.paginaC).style.backgroundColor = "white";
             self.paginaC= paginaNuova;
-            console.log("dopo #"+ self.paginaC);
+        },
+        Paginalogin: function(){
+            var self=this;
+            self.login=true;
         }
     },
     mounted() {
-        console.log("inizio");
         document.getElementById("Home").style.backgroundColor = "grey";
     }
 });
