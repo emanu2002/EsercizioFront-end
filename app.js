fe28b8b4afab53251f63efe7a9d37e27ed71c46d
Emanuele Carlucci <emanuele.carlucci100@edu.unito.it>
2023-03-09 13:08:07 +0100

    piccole modifiche

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ var app = new Vue({
     methods: {
         paginaCorrente: function(paginaNuova){
             var self=this;
-            document.getElementById(paginaNuova).style.backgroundColor = "grey";
-            document.getElementById(self.paginaC).style.backgroundColor = "white";
-            self.paginaC= paginaNuova;
+            if(paginaNuova != self.paginaC){
+                document.getElementById(paginaNuova).style.backgroundColor = "grey";
+                document.getElementById(self.paginaC).style.backgroundColor = "white";
+                self.paginaC= paginaNuova;
+            }
         },
         Paginalogin: function(){
             var self=this;
